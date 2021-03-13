813770c69eb4279656e5db4c7ba80bc6ca8fec82
Matt Ingham <matt.g.ingham@gmail.com>
2021-03-13 17:20:15 +0000

    EditForm addition

--- Shared/Palabra.cs
+++ Shared/Palabra.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace PalabrasApp.Shared
 {
     public class Palabra
     {
+        [Required]
         public string SpanishWord { get; set; }
+        [Required]
         public string EnglishWord { get; set; }
     }
 }
