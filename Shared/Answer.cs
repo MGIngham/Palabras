c0957b9d21480a2f44603c7097ceae6c8eac7233
Matt Ingham <matt.g.ingham@gmail.com>
2021-03-28 11:53:39 +0100

    Addition of styling and word check form

---
+++ Shared/Answer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalabrasApp.Shared
+{
+    public class Answer
+    {
+        public string AnswerText { get; set; }
+    }
+}
