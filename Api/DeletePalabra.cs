29696f7cf7a8d936bd2f3b97209efe0843961bb3
Matt Ingham <matt.g.ingham@gmail.com>
2021-05-23 19:15:15 +0100

    Route parameter added to DeletePalabra

--- Api/DeletePalabra.cs
+++ Api/DeletePalabra.cs
@@ -17,7 +17,7 @@ namespace PalabrasApp.Api
     public static class DeletePalabra
     {
         [FunctionName("DeletePalabra")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = null)] HttpRequest req,
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "/{id}")] HttpRequest req,
             [CosmosDB(
                 databaseName: "palabras",
                 collectionName: "ContainerMain",
