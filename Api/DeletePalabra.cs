d0bb0eed75fd6f3d76c53ad1b2b831e6e0188aaa
Matt Ingham <matt.g.ingham@gmail.com>
2021-05-23 18:37:33 +0100

    Commented out function

--- Api/DeletePalabra.cs
+++ Api/DeletePalabra.cs
@@ -28,11 +28,11 @@ namespace PalabrasApp.Api
                 ConnectionStringSetting = "PalabrasConnectionString")] DocumentClient client,
             Guid id)
         {
-            Uri palabraUri = UriFactory.CreateDocumentUri("palabras", "ContainerMain", id.ToString());
-            PartitionKey partitionKey = new PartitionKey("/Words");
-            RequestOptions requestOptions = new RequestOptions { PartitionKey = partitionKey };
+           // Uri palabraUri = UriFactory.CreateDocumentUri("palabras", "ContainerMain", id.ToString());
+            //PartitionKey partitionKey = new PartitionKey("/Words");
+            //RequestOptions requestOptions = new RequestOptions { PartitionKey = partitionKey };
 
-            ResourceResponse<Document> response = await client.DeleteDocumentAsync(palabraUri, requestOptions);
+            //ResourceResponse<Document> response = await client.DeleteDocumentAsync(palabraUri, requestOptions);
             // Use response for something or not..
 
             return new NoContentResult();
